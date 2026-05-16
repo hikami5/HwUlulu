8f1eb26c235967381e0fa45edb4229f75abe9982
hikami5 <ulianashtanko14@gmail.com>
2026-05-16 19:40:00 +0300

    .
    Refactor structs for Date and Student with default values.

--- structs.h
+++ structs.h
@@ -1,21 +1,19 @@
 #pragma once
-#include <iostream>
 
 typedef unsigned int uint;
 typedef unsigned short ushort;
 
-struct Date {
-    ushort day;
-    ushort month;
-    ushort year;
+struct Date
+{
+	ushort day;
+	ushort month;
+	ushort year;
 };
 
-struct Student {
-    char* name;
-    Date birthdayDate;
-    uint id;
-    float average;
+struct Student
+{
+	char* name = nullptr;
+	Date birthdayDate{ 0, 0, 0 };
+	uint id = 0;
+	float average = 0.0f;
 };
-
-void showStudent(const Student& std);
-void showAllStudents(const Student* students, uint size);
